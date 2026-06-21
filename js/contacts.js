3439f4b6b681281dcbb78a1857fce1f1484eab2a
ExClayyer <pokatilovdvp11@gmail.com>
2026-06-21 19:04:25 +0700

    Delete js directory

--- js/contacts.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('contactForm')?.addEventListener('submit', (e) => {
-    e.preventDefault();
-    alert('Спасибо! Ваше сообщение отправлено. Мы свяжемся с вами в ближайшее время.');
-    e.target.reset();
-});
--- js/home.js
+++
@@ -1,4 +0,0 @@
-window.addEventListener('scroll', function () {
-    var bg = document.querySelector('.parallax-bg');
-    if (bg) bg.style.transform = 'translateY(' + (window.scrollY * 0.4) + 'px)';
-});
